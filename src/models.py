bc66f590f95cd9b38f3b5cac1a92b336340146e0
Moises Navarro <moi55navacor@gmail.com>
2021-02-23 17:44:51 +0000

    Firts Commit

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,26 +8,43 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+    User_Id = Column(Integer, primary_key = True)
+    Nickname = Column(String(20))
+    Password = Column(String(20))
+    Name = Column(String(20))
+    Last_Name = Column(String(40))
+    Birth_Date = Column(Date)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Characters(Base):
+    __tablename__ = 'characters'
+    Character_Id = Column(Integer, primary_key = True)
+    Name = Column(String(20))
+    Birth = Column(Date)
+    Gender = Column(String(10))
+    Height = Column(Integer) 
+    Skin_Color = Column(String(20))
+    Eye_Color = Column(String(20))
+    Description = Column(String (2000))
 
-    def to_dict(self):
-        return {}
+class Planets(Base):
+    __tablename__ = 'planets'
+    Planet_Id = Column(Integer, primary_key = True)
+    Name = Column(String(20))
+    Climate = Column(String(20))
+    Population = Column(Integer)
+    Orbital_Period = Column(Integer)
+    Rotation_Period = Column(Integer)
+    Diamer = Column(Integer)
+    Description = Column(String (2000))
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    Fav_Id = Column(Integer, primary_key = True)
+    Favorite = Column(String(20))
+    User_Id = Column(Integer, ForeignKey('user.User_Id'))
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
